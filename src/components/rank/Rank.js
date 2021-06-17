5edd05e21c2005929bcf99f0e865239b2d875052
Jeffrey Zhang <pengtuzhang@gmail.com>
2021-06-17 17:24:44 +1000

    something

---
+++ src/components/rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const Rank = () =>{
+    return(
+        <div>
+           <div className='white f3 center'>
+                {'肥猪，你现在的排名是：'}
+           </div>
+           <div className='white f1'>
+                {'第5名'}
+           </div>
+        </div>
+    );
+}
+
+export default Rank;
